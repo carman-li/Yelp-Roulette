adf4c620b2e40aad279fc290fb4c124ab5742ac9
Carman Li <licarman6962@gmail.com>
2020-05-05 18:00:46 -0400

    Updates

---
+++ precache-manifest.9e0691adfadba387eeab34dd226eae00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541940cf81d772be3c12459937873246",
+    "url": "/yelp-roulette/index.html"
+  },
+  {
+    "revision": "248e46ce3ebd518052dc",
+    "url": "/yelp-roulette/static/css/main.db2e37d7.chunk.css"
+  },
+  {
+    "revision": "b8e6c286fc4d48bbbe49",
+    "url": "/yelp-roulette/static/js/2.40dfef02.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/yelp-roulette/static/js/2.40dfef02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248e46ce3ebd518052dc",
+    "url": "/yelp-roulette/static/js/main.15ad8470.chunk.js"
+  },
+  {
+    "revision": "92a47352e01fd34d887b",
+    "url": "/yelp-roulette/static/js/runtime-main.543c0195.js"
+  }
+]);
